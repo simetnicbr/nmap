c62b401db5c4de6ee60a82eaf8c9443d877baf85
Hilko Bengen <bengen@debian.org>
2017-09-09 11:26:50 +0200

    Put autoreconf call into debian/rules

--- debian/autoreconf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# only perform autoreconf on the parts that are used for the build
-for DIR in . libdnet-stripped nbase ncat nmap-update nping nsock/src ; do
-    autoreconf -fi --no-recursive "$DIR"
-done
-
