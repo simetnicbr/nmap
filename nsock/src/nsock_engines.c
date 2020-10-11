371c523905de8e6da735905c4ba25f9109fbf2ec
Hilko Bengen <bengen@debian.org>
2020-10-11 23:59:25 +0200

    New upstream version 7.90+dfsg1

--- nsock/src/nsock_engines.c
+++ nsock/src/nsock_engines.c
@@ -6,7 +6,7 @@
  *                                                                         *
  ***********************IMPORTANT NSOCK LICENSE TERMS***********************
  *                                                                         *
- * The nsock parallel socket event library is (C) 1999-2019 Insecure.Com   *
+ * The nsock parallel socket event library is (C) 1999-2020 Insecure.Com   *
  * LLC This library is free software; you may redistribute and/or          *
  * modify it under the terms of the GNU General Public License as          *
  * published by the Free Software Foundation; Version 2.  This guarantees  *
@@ -55,7 +55,7 @@
  *                                                                         *
  ***************************************************************************/
 
-/* $Id: nsock_engines.c 37640 2019-05-28 21:36:04Z dmiller $ */
+/* $Id: nsock_engines.c 38071 2020-10-02 05:02:05Z fyodor $ */
 
 #ifdef HAVE_CONFIG_H
 #include "nsock_config.h"
@@ -100,8 +100,8 @@ extern struct io_engine engine_select;
 static struct io_engine *available_engines[] = {
   ENGINE_EPOLL
   ENGINE_KQUEUE
-  ENGINE_POLL
   ENGINE_IOCP
+  ENGINE_POLL
   ENGINE_SELECT
   NULL
 };
